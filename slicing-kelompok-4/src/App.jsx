e3bfa4617d9a02072ec55a6f210ae5ed7476b621
achmadzahrul01 <achmadzahrul01@gmail.com>
2024-11-12 16:32:54 +0700

    initial commit

---
+++ slicing-kelompok-4/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='bg-black'>Hello, React!</h1>
+      <p className=' font-raleway'>text peach red</p>
+      <p>text peach red</p>
+    </div>
+  );
+};
+
+export default App;
---
+++ slicing-kelompok-4/tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+    fontFamily: {
+      // Font kustom
+      'raleway': ['Raleway', 'sans-serif'],
+    },
+    colors: {
+      // Warna kustom
+      darkblue: '#0E1F51',
+      peachred: '#FF3E54',
+      blackshade: '#252525',
+      offwhite: '#F7F7F7',
+      whitecstm: '#081717',
+    },
+  },
+  plugins: [],
+};
