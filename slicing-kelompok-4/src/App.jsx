fc910d403faae847d1c6f37fb9de0baf024c5153
achmadzahrul01 <achmadzahrul01@gmail.com>
2024-11-13 10:31:54 +0700

    feat(App): Combine Routing

--- slicing-kelompok-4/src/App.jsx
+++ slicing-kelompok-4/src/App.jsx
@@ -1,30 +1,21 @@
-<<<<<<< HEAD
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import About from './pages/About';
+import Contact from './pages/Contact';
 import Footer from './components/Footer';
-
-const App = () => {
-  return (
-    <div>
-      <Footer />
-    </div>
-=======
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import About from "./pages/About";
-import Contact from "./pages/Contact";
 
 const App = () => {
   return (
     <Router>
       {/* <Navbar /> */}
-        <Routes>
-          <Route path="/" element={<About />} />
-          {/* <Route path="/" element={<Contact />} /> */}
-        </Routes>
-      
+      <Routes>
+        <Route path="/" element={<About />} />
+        {/* <Route path="/" element={<Contact />} /> */}
+      </Routes>
+      <Footer />
     </Router>
->>>>>>> dev-oryza
   );
 };
 
 export default App;
+
