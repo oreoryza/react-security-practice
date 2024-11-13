558e8ac5f0d5eed7786122e65fa3c5ff5b5cc855
achmadzahrul01 <achmadzahrul01@gmail.com>
2024-11-13 10:22:42 +0700

    feat(App, OurServices) Completing Content OurServices & Add routing for OurServices on App.jsx

--- slicing-kelompok-4/src/App.jsx
+++ slicing-kelompok-4/src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import OurServices from './pages/OurServices';
 import Footer from './components/Footer';
 
 const App = () => {
   return (
     <div>
+      <OurServices />
       <Footer />
     </div>
   );
